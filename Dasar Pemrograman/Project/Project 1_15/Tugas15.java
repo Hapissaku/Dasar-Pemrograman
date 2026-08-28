a8383e219904a1c4ecf13d379f1cc50e1dbf792f
Hafizh Arkana Putra Adiansyah <hypeaxiooamd@gmail.com>
2026-08-28 12:56:18 +0700

    1

---
+++ Dasar Pemrograman/Project/Project 1_15/Tugas15.java
@@ -0,0 +1,22 @@
+public class Tugas15 {
+    static void main() {
+        //Kondisi awal
+        System.out.println("nampan A berisi 1 bola bintang");
+        System.out.println("nampan B berisi 1 bola bulan");
+        System.out.println("nampan C kosong");
+        //Percobaan robot
+        System.out.println("\n1) Ambil bola di nampan A, taruh di nampan C");
+        System.out.println("2) Ambil bola di nampan B, taruh di nampan A");
+        System.out.println("3) Ambil bola di nampan C, taruh di nampan B");
+        //hasil
+        System.out.println("\nSetelah langkah 1 :");
+        System.out.println("Nampan A : kosong B : bulan C : Bintang");
+        System.out.println("\nSetelah langkah 2 :");
+        System.out.println("Nampan A : bulan B : kosong C : Bintang");
+        System.out.println("\nSetelah langkah 3 :");
+        System.out.println("Nampan A : bulan B : bintang C : kosong");
+        //Kesimpulan
+        System.out.println("\nKesimpulan : ");
+        System.out.println("Jawaban benar adalah  (a) Kedua bola sudah bertukar tempat &  (e) nampan C kosong");
+    }
+}
