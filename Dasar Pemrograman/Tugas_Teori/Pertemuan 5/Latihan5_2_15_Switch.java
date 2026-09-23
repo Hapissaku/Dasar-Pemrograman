53283e8366e57e746c84d22e9825576bdfdd7aa9
Hafizh Arkana Putra Adiansyah <hypeaxiooamd@gmail.com>
2026-09-23 09:21:36 +0700

    Add files via upload

---
+++ Dasar Pemrograman/Tugas_Teori/Pertemuan 5/Latihan5_2_15_Switch.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Latihan5_2_15_Switch {
+    public static void main(String[] args) {
+        
+   int nomor;     
+
+   Scanner input  = new Scanner(System.in);
+   System.out.print("Masukkan Input Nomor (1-4) = ");
+   nomor = input.nextInt();
+
+    switch(nomor){
+        case 1:
+            System.out.println("Legalisir Ijazah, Loket A");
+            break;
+         case 2:
+            System.out.println("Surat Keterangan Aktif Kuliah, Loket B");
+            break;
+        case 3:
+            System.out.println("Pebayaran UKT, Loket C");
+            break;
+         case 4:
+            System.out.println("Pengajuan Cuti Akademik, Loket D");
+            break;
+        default :
+            System.out.println("Nomor Tidak Valid");
+
+
+}
+}
+}
